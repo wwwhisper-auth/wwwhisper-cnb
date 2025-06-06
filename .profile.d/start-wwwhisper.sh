f0cc6e138d40f1df4b76daba8c150e479210f0a2
Jan Wrobel <jan@mixedbit.org>
2025-06-06 11:19:51 +0200

    Initial version of the wwwhisper CNB buildpack

---
+++ .profile.d/start-wwwhisper.sh
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+#
+# 1) Replaces the TCP $PORT exported to the app with a TCP port not
+# available from the outside world. Heroku platform guarantees that
+# only the web $PORT can be reached from the outside: 'The Common
+# Runtime provides strong isolation by firewalling all dynos off from
+# one another. The only traffic that can reach a dyno is web requests
+# forwarded from the router to web processes listening on the port
+# number specified in the $PORT environment variable.'
+# https://devcenter.heroku.com/articles/dynos#common-runtime-networking
+#
+# 2) Starts auth proxy in background to listen on the original $PORT
+# and to authenticate and authorize requests. Authorized requests are
+# proxied to the web application.
+#
+#
+# If this startup script fails, the whole app startup process
+# correctly crashes, which ensures that app with the wwwhisper
+# buildpack will not be started on the original port (this would
+# bypass authorization).
+
+
+# .profile.d scripts are also sourced by `heroku ps:exec`, in such
+# case $PORT is not set and this script should do nothing.
+if [[ -z "${PORT}" ]]
+then
+  return 0
+fi
+
+WWWHISPER_PID_FILE=`pwd`/wwwhisper/logs/wwwhisper.pid
+
+function wwwhisper_log() {
+  echo "buildpack=wwwhisper $*"
+}
+
+function wwwhisper_fatal() {
+  wwwhisper_log "$*"
+  exit 1
+}
+
+# When the dyno manager restarts a dyno, it sends SIGTERM to all
+# processes in the dyno
+# (https://devcenter.heroku.com/articles/dyno-shutdown-behavior)
+# When the SIGTERM is delivered, wait until proxy terminates and then
+# re-raise the SIGTERM to terminate the current process.
+function wwwhisper_sigterm_handler() {
+  wwwhisper_log "SIGTERM received, waiting for auth proxy to terminate."
+  # auth proxy removes the pid file when it exits.
+  while [[ -f ${WWWHISPER_PID_FILE} ]] ; do
+    sleep 0.2
+  done
+  wwwhisper_log "auth proxy terminated."
+  # Remove the trap and re-raise the signal
+  trap - SIGTERM
+  kill -SIGTERM $$
+}
+
+function wwwhisper_main() {
+  local public_port=${PORT}
+  # Web app should use a private port, not accessible externally.
+  # Reasign the PORT.
+  if [[ "${public_port}" != "17080" ]]; then
+    export PORT=17080
+  else
+    export PORT=17081
+  fi
+  wwwhisper_log "Remapped web app external port to private port ${PORT}."
+
+  local web_app_pid=$$
+
+  if [[ -z "${WWWHISPER_URL}" ]]; then
+    wwwhisper_fatal 'wwwhisper add-on must be enabled to use this buildpack.'
+  fi
+
+  function wwwhisper_run_auth_proxy() {
+    wwwhisper_log "Waiting for the app to start listening on port ${PORT}."
+
+    # Loop without any time limit, but Heroku will kill the dyno if
+    # it doesn't bind the external port in 60 seconds.
+    while ! nc -z localhost ${PORT}; do
+      sleep 0.2
+    done
+
+    # Do not terminate the subshell with SIGTERM, let the proxy handle
+    # this signal and terminate gracefully which then terminates the
+    # subshell.
+    trap "" SIGTERM
+
+    wwwhisper_log "Staring wwwhisper auth proxy."
+    ./wwwhisper/bin/wwwhisper -listen ${public_port} -proxyto ${PORT} -pidfile ${WWWHISPER_PID_FILE} &
+
+    local proxy_pid="$!"
+
+    wait -f ${proxy_pid}
+    local exit_code=$?
+
+    # Dyno manager monitors web app process, not the auth proxy
+    # process, so when proxy fails, we terminate web app process. This
+    # way dyno manager notices dyno failure and restarts the dyno (it
+    # would eventually do it anyway, because $PORT stops accepting
+    # requests, but terminating web app process makes it quicker).
+    #
+    # If auth proxy is terminated by Heroku dyno manager (SIGTERM or
+    # SIGQUIT), exit code is 0 and we don't resend this signal,
+    # because the dyno manager already delivers it to all the
+    # processes.
+    if (( ${exit_code} != 0 )); then
+      wwwhisper_log "auth proxy failed with code ${exit_code}," \
+                    "killing web app with SIGTERM."
+      # In case proxy crashed without removing the pid file.
+      rm -f ${WWWHISPER_PID_FILE}
+      kill -SIGTERM ${web_app_pid} >/dev/null
+      if (( $? == 0 )); then
+        # SIGTERM was delivered successfully, deliver SIGKILL after some time.
+        sleep 40
+        wwwhisper_log "Killing web app with SIGKILL."
+        kill -SIGKILL ${web_app_pid} >/dev/null
+      fi
+    fi
+
+    wwwhisper_log "exiting"
+    exit 1
+  }
+
+  trap wwwhisper_sigterm_handler SIGTERM
+  wwwhisper_run_auth_proxy &
+}
+
+wwwhisper_main
+
+# Do not pass WWWHISPER_URL to the app. This is in case wwwhisper
+# buildpack is enabled for an app that already uses wwwhisper
+# middleware for node.js or Ruby. Lack of WWWHISPER_URL will cause the
+# middleware to show an error (such setup doesn't make sense, as it
+# would make two auth requests per each incomming request).
+unset WWWHISPER_URL
+
